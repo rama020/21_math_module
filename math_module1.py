22e285c934ed2132be27377120c0182dd402a126
rama020 <118764128+rama020@users.noreply.github.com>
2022-12-14 22:32:13 +0530

    Add files via upload

---
+++ math_module1.py
@@ -0,0 +1,21 @@
+'''
+DATE:13TH DEC 2022
+DAY: TUESDAY
+TOPIC: MATH MODULE
+AUTHOR: POOJA
+'''
+import math
+n=[1,3,5]
+print(math.fsum(n)) #9.0
+print(math.prod(n))#15
+h=12%3
+print(h) #0
+print(math.fmod(12,3))#0.0
+u=-4
+print(abs(u))#4
+print(math.fabs(-5))#5.0
+print(math.trunc(2.31))#2
+print(math.trunc(-3.2))#-3
+print(math.gcd(15,11))#1
+print(math.lcm(3,15))#15
+print(abs(-4+2))#2
